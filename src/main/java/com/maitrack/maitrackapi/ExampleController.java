7b0375879c7ca33593ffd28176524b737a05d814
mpatel <maitrapatel@yahoo.com>
2021-04-27 13:52:38 -0400

    Test Commit 4/27

--- src/main/java/com/maitrack/maitrackapi/ExampleController.java
+++ src/main/java/com/maitrack/maitrackapi/ExampleController.java
@@ -3,6 +3,7 @@ package com.maitrack.maitrackapi;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//Just an example
 @Controller
 public class ExampleController {
     @RequestMapping("/email_template")
